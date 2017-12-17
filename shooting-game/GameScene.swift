7398244c36bf498e115b7bcb7101fabd8fc3e9fd
Masaya Hayashi <mcdonalds.only@gmail.com>
2017-12-17 16:39:17 +0900

    Introduce score system

--- shooting-game/GameScene.swift
+++ shooting-game/GameScene.swift
@@ -16,6 +16,11 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
     var accelaration: CGFloat = 0.0
 
     var timer: Timer?
+    var score: Int = 0 {
+        didSet {
+            scoreLabel.text = "Score: \(score)"
+        }
+    }
 
     let spaceshipCategory: UInt32 = 0b0001
     let missileCategory: UInt32 = 0b0010
@@ -25,6 +30,7 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
     var earth: SKSpriteNode!
     var spaceship: SKSpriteNode!
     var hearts: [SKSpriteNode] = []
+    var scoreLabel: SKLabelNode!
 
     override func didMove(to view: SKView) {
         physicsWorld.gravity = CGVector(dx: 0, dy: 0)
@@ -67,6 +73,12 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
             addChild(heart)
             hearts.append(heart)
         }
+
+        scoreLabel = SKLabelNode(text: "Score: 0")
+        scoreLabel.fontName = "Papyrus"
+        scoreLabel.fontSize = 50
+        scoreLabel.position = CGPoint(x: -frame.width / 2 + scoreLabel.frame.width / 2 + 50, y: frame.height / 2 - scoreLabel.frame.height * 5)
+        addChild(scoreLabel)
     }
 
     override func didSimulatePhysics() {
@@ -131,6 +143,7 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         asteroidNode.removeFromParent()
         if target.categoryBitMask == missileCategory {
             targetNode.removeFromParent()
+            score += 5
         }
 
         self.run(SKAction.wait(forDuration: 1.0)) {
